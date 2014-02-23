74ddde979d2c000d565660800826a8479f064415
francescoagati <francescoagati1975@gmail.com>
2014-02-23 19:20:21 +0100

    wip

--- lib/veespo/api.rb
+++ lib/veespo/api.rb
@@ -8,16 +8,16 @@ module Veespo
 
   class Api
     class << self
-      def self.get_token(params={})
+      def get_token(params={})
         Api.http_get("/v1/auth/category/#{params[:category]}/user-token", api_key:params[:api_key],user:params[:user])['token']
       end
 
-      def self.http_get(api_path,path,params)
+      def http_get(api_path,path,params)
         conn = connection(api_path) 
         JSON.parse(conn.get(path,params).body)['data']
       end
 
-      def self.connection(api_path) 
+      def connection(api_path) 
         Faraday.new(api_path) do |c|
           c.use Faraday::Response::RaiseError
           c.use Faraday::Adapter::NetHttp
